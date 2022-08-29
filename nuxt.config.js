272f727d57f7aaeed698fab189863ead53bc210f
Guillermo Croppi <guillermocroppi@gmail.com>
2022-08-29 19:11:43 -0300

    Update

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,14 +7,14 @@ export default {
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Data Visualizations - IVE',
+    title: 'Mapa dinámico IVE',
     htmlAttrs: {
       lang: 'es'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Multiple data visualizations - IVE Argentina' },
+      { hid: 'description', name: 'description', content: 'Mapa dinámico IVE - MMGYD' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
